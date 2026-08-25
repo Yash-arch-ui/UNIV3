db02557b50fa13febefeba29b2774a17c2cde8a4
Yash_10 <yashyt01@gmail.com>
2026-08-25 17:07:08 +0000

    style: normalize Router formatting and consistent code style
    - Fix inconsistent whitespace and indentation
    - Normalize spacing in constructor and function bodies
    - Add trailing newline at end of file

--- src/Router.sol
+++ src/Router.sol
@@ -5,38 +5,39 @@ import "./interfaces/ICLAMMPOOL.sol";
 import "./interfaces/IPositionManager.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Router{
-    address public factory ;
+contract Router {
+    address public factory;
     address public positionManager;
 
-constructor(address _factory, address _positionManager ){
-    factory = _factory;
-    positionManager = _positionManager;
+    constructor(address _factory, address _positionManager) {
+        factory = _factory;
+        positionManager = _positionManager;
+    }
+
+    function swapExactTokensForTokens(address tokenIn, address tokenOut, uint256 amountIn, bool zeroForOne) external {
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        address pool = ICLAMMFactory(factory).getPool(tokenIn, tokenOut);
+        require(pool != address(0), "POOL_NOT_FOUND");
+        IERC20(tokenIn).approve(pool, amountIn); // approving pool to use
+        ICLAMMPool(pool).swap(zeroForOne, amountIn);
+    }
+
+    function mintPosition(int24 tickLower, int24 tickUpper, uint128 liquidity) external {
+        IPositionManager(positionManager).mint(msg.sender, tickLower.tickUpper, liquidity);
+    }
+
+    function increaseLiquidity(uint256 tokenId, uint128 liquidity) external {
+        IPositionManager(positionManager).increaseLiquidity(msg.sender, tokenId, liquidity);
+    }
+
+    function decreaseLiquidity(uint256 tokenId, uint128 liquidity) external {
+        IPositionManager(positionManager).decreaseLiquidity(msg.sender, tokenId, liquidity);
+    }
+
+    function collect(uint256 tokenId) external {
+        IPositionManager(positionManager).collect(msg.sender, tokenId);
+    }
 }
 
-function swapExactTokensForTokens(address tokenIn, address tokenOut, uint256 amountIn, bool zeroForOne) external{
-    IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
-    address pool = ICLAMMFactory(factory).getPool(tokenIn,tokenOut);
-    require(pool != address(0), "POOL_NOT_FOUND");
-    IERC20(tokenIn).approve(pool, amountIn);// approving pool to use 
-    ICLAMMPool(pool).swap(zeroForOne, amountIn);
-}
-
-function mintPosition(int24 tickLower, int24 tickUpper, uint128 liquidity) external {
-    IPositionManager(positionManager).mint(msg.sender,tickLower. tickUpper, liquidity);
-}
-
-function increaseLiquidity(uint256 tokenId, uint128 liquidity) external {
-    IPositionManager(positionManager).increaseLiquidity(msg.sender,tokenId, liquidity);
-    
-}
-function decreaseLiquidity(uint256 tokenId, uint128 liquidity) external {
-    IPositionManager(positionManager).decreaseLiquidity(msg.sender,tokenId, liquidity);
-}
-function collect (uint256 tokenId) external {
-    IPositionManager(positionManager).collect(msg.sender, tokenId);
-}
-}
-
-// POOL - FACTORY 
-// LP POS - PositionManager 
+// POOL - FACTORY
+// LP POS - PositionManager
