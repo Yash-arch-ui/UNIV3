a9fa75d8a6a09de3de67bb459836429c041178b6
Yash_10 <yashyt01@gmail.com>
2026-08-25 17:07:32 +0000

    style: normalize IPositionManager interface formatting
    - Fix inconsistent whitespace and indentation
    - Normalize function signature formatting
    - Add trailing newline at end of file

--- src/interfaces/IPositionManager.sol
+++ src/interfaces/IPositionManager.sol
@@ -1,5 +1,6 @@
 //SPDX-License-identifier:MIT
 pragma solidity ^0.8.19;
-interface IPositionManager{
-    function mint(address owner, int24 tickUpper, int24 tickLower, uint128 liquidity) external; 
+
+interface IPositionManager {
+    function mint(address owner, int24 tickUpper, int24 tickLower, uint128 liquidity) external;
 }
