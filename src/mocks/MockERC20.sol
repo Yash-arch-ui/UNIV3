67ea42238098d28195a47ef31322e7c6ede28fd3
Yash_10 <yashyt01@gmail.com>
2026-08-25 17:08:21 +0000

    style: normalize MockERC20 mock formatting
    - Fix inconsistent whitespace and indentation
    - Consolidate constructor to single line
    - Normalize function signature formatting
    - Add trailing newline at end of file

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -3,16 +3,9 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC20(name, symbol) {}
-
-    function mint(
-        address to,
-        uint256 amount
-    ) external {
+    function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
 }
