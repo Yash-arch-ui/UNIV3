f88e347c10eb465d9f075faca42f3307e59e2690
Yash_10 <yashyt01@gmail.com>
2026-08-25 17:07:28 +0000

    style: normalize IFlashCallback interface formatting
    - Fix inconsistent whitespace and indentation
    - Remove trailing semicolon spacing
    - Add trailing newline at end of file

--- src/interfaces/IFlashCallback.sol
+++ src/interfaces/IFlashCallback.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
-interface IFlashCallback{
-    function flashCallback(uint256 fee0, uint256 fee1 , bytes calldata data) external ;
+
+interface IFlashCallback {
+    function flashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external;
 }
